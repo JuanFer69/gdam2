fb476074cd1da2b545fae4e6e6fee4307c719d64
HP196\CES <informatica@escuelaces.com>
2022-05-25 11:56:07 +0200

    Comit inicial

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -19,8 +19,33 @@ public class Agenda {
 
     private HashMap<String, Contacto> contactos = new HashMap();
     private static int n = 0;
-    private static int count = 0;
- 
+    private static int count = 0; 
+    
+    
+    private static String nombreAgenda="";
+
+    public String getnombreAgenda(Agenda a){
+            return this.nombreAgenda;
+    }
+    public void setnombreAgenda(String name){
+            this.nombreAgenda=name;
+    }
+
+    public Agenda(String name){
+    
+        if (name != ""){
+            this.nombreAgenda = name;
+        }
+        else this.nombreAgenda ="Untitled";
+    }
+ /**
+  * Añade contacto a la agenda 
+  * @param contacto  contacto que queremos añadir 
+  * @return Objeto contacto añadido o genera un excepcion si ya existia
+  * @throws ContactoRepetidoException    Excepcion programada , ver paquete excepcion
+  */
+    
+    
     public Contacto addContacto(Contacto contacto) throws ContactoRepetidoException {
         if (contactos.containsKey(contacto.getEmail())) {
             //
@@ -32,7 +57,11 @@ public class Agenda {
         count++;
         return contacto;
     }
-
+/**
+ * Elimina un contacto de la agenda
+ * @param email Contacto que qeuremos eliminar
+ * @return Muestra el contacto elimiinado
+ */
     public Contacto eliminarContacto(String email) { 
         Contacto contacto = buscarContacto(email);
         if (contacto != null) {
@@ -52,7 +81,10 @@ public class Agenda {
         return contactos.get(email);
         }
     }
-
+/**
+ * Buscar al conttacot en la agenda
+ * @return  Muestra al cntacto que deseabas encontrar
+ */
     public ArrayList<Contacto> getContactos() {
         ArrayList<Contacto> contactosList = new ArrayList<>();
         contactosList.addAll(contactos.values());
